695ea48c176d7357b56b377fc6499a49b9977185
s26288-pj <92212395+s26288-pj@users.noreply.github.com>
2023-06-24 15:09:16 +0200

    Initial commit

---
+++ src/main/java/com/example/jazs26288nbp/JazS26288NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs26288nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS26288NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS26288NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jazs26288nbp/JazS26288NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs26288nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS26288NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
